18647861c440b1decb05b1aef75504965aaa91aa
Izabela <izabelaszeremeta@gmail.com>
2023-01-23 19:00:53 -0600

    minimum requirement

---
+++ src/Bedroom.jsx
@@ -0,0 +1,9 @@
+function Bedroom(props) {
+  return (
+    <div>
+      <h2>Bedroom {props.bedNum} </h2>
+    </div>
+  );
+}
+
+export default Bedroom;
---
+++ src/LivingRoom.jsx
@@ -0,0 +1,9 @@
+function LivingRoom(props) {
+  return (
+    <div>
+      <h2>Living Room</h2>
+    </div>
+  );
+}
+
+export default LivingRoom;
